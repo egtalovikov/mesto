383345762ba5ec0986aa0e7e339b46a10679ffb0
Egor Talovikov <egtalovikov@gmail.com>
2022-08-16 13:52:07 +0300

    feature: добавить удаление карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
-let popups = document.querySelectorAll('.popup');
-let popupEdit = document.querySelector('.popup_edit');
-let popupAdd = document.querySelector('.popup_add');
-let editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
-let closeButtons = document.querySelectorAll('.popup__close-button');
-let submitButton = document.querySelector('.form__submit-button');
-let editFormElement = document.querySelector('.form_edit');
-let addFormElement = document.querySelector('.form_add');
-let nameInput = document.querySelector('.form__text_type_name');
-let jobInput = document.querySelector('.form__text_type_bio');
-let profileName = document.querySelector('.profile__name');
-let profileBio = document.querySelector('.profile__bio');
+const popups = document.querySelectorAll('.popup');
+const popupEdit = document.querySelector('.popup_edit');
+const popupAdd = document.querySelector('.popup_add');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
+const submitButton = document.querySelector('.form__submit-button');
+const editFormElement = document.querySelector('.form_edit');
+const addFormElement = document.querySelector('.form_add');
+const nameInput = document.querySelector('.form__text_type_name');
+const jobInput = document.querySelector('.form__text_type_bio');
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
 const postsContainer = document.querySelector('.posts');
 const initialPosts = [
   {
@@ -44,25 +44,25 @@ function openPopupAdd() {
 }
 
 function openPopupEdit() {
-  nameInput.value= profileName.textContent;
+  nameInput.value = profileName.textContent;
   jobInput.value = profileBio.textContent;
   popupEdit.classList.add('popup_opened');
 }
 
 
 function closePopup() {
-  popups.forEach(function(el, i) {
+  popups.forEach(function (el, i) {
     popups[i].classList.remove('popup_opened');
   });
 };
 
-function editFormSubmitHandler (evt) {
-    evt.preventDefault();
+function editFormSubmitHandler(evt) {
+  evt.preventDefault();
 
-    profileBio.textContent = jobInput.value;
-    profileName.textContent = nameInput.value;
+  profileBio.textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
 
-    closePopup();
+  closePopup();
 }
 
 function addPost(name, link) {
@@ -70,6 +70,9 @@ function addPost(name, link) {
   const postElement = postTemplate.querySelector('.post').cloneNode(true);
 
   const photo = postElement.querySelector('.post__photo');
+  postElement.querySelector('.post__delete-button').addEventListener('click', function(evt){
+    postElement.remove();
+  })
   photo.setAttribute('src', link);
   postElement.querySelector('.post__title').textContent = name;
   postElement.querySelector('.post__like').addEventListener('click', function (evt) {
@@ -79,7 +82,7 @@ function addPost(name, link) {
   postsContainer.prepend(postElement);
 }
 
-function addFormSubmitHandler (evt) {
+function addFormSubmitHandler(evt) {
   evt.preventDefault();
 
   const postName = document.querySelector('.form__text_type_post-name');
@@ -94,10 +97,10 @@ addFormElement.addEventListener('submit', addFormSubmitHandler);
 editFormElement.addEventListener('submit', editFormSubmitHandler);
 editButton.addEventListener('click', openPopupEdit);
 addButton.addEventListener('click', openPopupAdd);
-closeButtons.forEach(function(el, i) {
+closeButtons.forEach(function (el, i) {
   closeButtons[i].addEventListener('click', closePopup);
 });
 
-initialPosts.forEach(function(element) {
+initialPosts.forEach(function (element) {
   addPost(element.name, element.link);
 });
