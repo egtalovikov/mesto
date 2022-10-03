0578720fd2ff31245ecab7a94402017a86f47b84
Egor Talovikov <egtalovikov@gmail.com>
2022-10-03 15:11:33 +0300

    feat: добавил класс Popup

---
+++ scripts/Popup.js
@@ -0,0 +1,31 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popupSelector = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popupSelector.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popupSelector.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close(this._popupSelector);
+    }
+  }
+
+  setEventListeners() {
+    this._popupSelector.addEventListener('click', this._closePopupByClickingOverlay);
+  }
+
+  _closePopupByClickingOverlay(evt) {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-button')) {
+      this.close(evt.currentTarget);
+    }
+  };
+}
